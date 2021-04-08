802d4bbf3161d7868d40b9e77332c4d971bc37f1
Vladlen <ilusenkovladlen@gmail.com>
2021-04-08 11:53:48 +0300

    Redisigned UserMenu

--- src/com/innsmouth/library/controller/users/UserMenuEditController.java
+++ src/com/innsmouth/library/controller/users/UserMenuEditController.java
@@ -1,4 +1,14 @@
 package com.innsmouth.library.controller.users;
 
+import javafx.event.ActionEvent;
+
 public class UserMenuEditController {
+    public void onDeletePressed(ActionEvent actionEvent) {
+    }
+
+    public void onBackPressed(ActionEvent actionEvent) {
+    }
+
+    public void onConfirm(ActionEvent actionEvent) {
+    }
 }
