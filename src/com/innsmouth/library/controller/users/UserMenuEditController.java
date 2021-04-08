ffd8b5fbb2c84aa9d0446ab5bbcc604ea4593eae
Vladlen <ilusenkovladlen@gmail.com>
2021-04-08 11:37:11 +0300

    Added UserMenu + Controllers

---
+++ src/com/innsmouth/library/controller/users/UserMenuEditController.java
@@ -0,0 +1,4 @@
+package com.innsmouth.library.controller.users;
+
+public class UserMenuEditController {
+}
---
+++ src/com/innsmouth/library/controller/users/UserMenuInformController.java
@@ -0,0 +1,11 @@
+package com.innsmouth.library.controller.users;
+
+import javafx.event.ActionEvent;
+
+public class UserMenuInformController {
+    public void onEditUser(ActionEvent actionEvent) {
+    }
+
+    public void onBackPressed(ActionEvent actionEvent) {
+    }
+}
